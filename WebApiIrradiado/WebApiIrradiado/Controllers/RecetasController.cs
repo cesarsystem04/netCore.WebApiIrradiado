3942eec51b9186edf86b4673f5bb8e0ee254663d
César Torres <cesarsystem04@gmail.com>
2023-03-29 13:22:33 -0600

    Primera versión del proyecto.
    Se sube la primera versión del proyecto.

---
+++ WebApiIrradiado/WebApiIrradiado/Controllers/RecetasController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiIrradiado.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecetasController : Controller
+    {
+
+        // POST: RecetasController/Create
+        [HttpPost]
+        [Route("AddReceta")]
+        public ActionResult AddReceta(Recetas receta)
+        {
+            try
+            {
+                RecetasResponse recetas = new RecetasResponse();
+                recetas.recetas = receta;
+                recetas.codError = 0;
+                recetas.msgRespuesta = "Ejecución exitosa";
+
+                return Ok(recetas);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPost]
+        [Route("PutReceta")]
+        public ActionResult PutReceta(RecetasRequest request)
+        {
+            try
+            {
+                RecetasResponse recetas = new RecetasResponse();
+                recetas.recetas = request.data.recetas[0] ?? null;
+                recetas.codError = 0;
+                recetas.msgRespuesta = "Ejecución exitosa";
+
+                return Ok(recetas);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+
+    }
+}
---
+++ WebApiIrradiado/WebApiIrradiado/Recetas.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApiIrradiado
+{
+    public class Recetas
+    {
+        public string? recipe_num { get; set; }
+        public string? empaque { get; set; }
+        public string? parametro { get; set; }
+        public decimal nom_value { get; set; }
+        public string? uom { get; set; }
+        public decimal min_value { get; set; }
+        public decimal max_value { get; set; }
+        public byte manual { get; set; }
+        public byte detenido { get; set; }
+        public string? tipo { get; set; }
+        public byte trans_erp { get; set; }
+        public string? date_modify { get; set; }
+
+
+    }
+
+    public class RecetasResponse
+    {
+        public Recetas? recetas { get; set; }
+        public string? msgRespuesta { get; set; }
+        public int codError { get; set; }
+
+
+
+    }
+
+
+    public class RecetasRequest
+    {
+        public Data? data { get; set; }
+
+
+    }
+
+
+    public class Data 
+    {
+        public Recetas[]? recetas { get; set; }
+
+    }
+
+    
+
+}
